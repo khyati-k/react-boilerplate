7928ea43c2a1e0132e6d6933c62e7ee3676ca482
Khyati Kulshreshtha <khyati.kul@gmail.com>
2018-04-24 19:34:11 +0400

    no routing yet

---
+++ app/client.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//local dependencies
+
+class App extends React.Component {
+//state and methods    
+    render() {
+        return (
+            <div>Hi</div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, root);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: __dirname + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
+module.exports = {
+	entry: [
+		'./app/client.js'
+	],
+	output: {
+		path: __dirname + '/docs',
+		filename: "client_bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['env']
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [HTMLWebpackPluginConfig],
+	mode: 'development'
+}
