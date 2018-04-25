3b42d4d76f72b793a2438ed4aa95af9e9e3be5c1
Khyati Kulshreshtha <khyati.kul@gmail.com>
2018-04-25 12:36:39 +0400

    Added directories

--- app/client.js
+++ app/client.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//local dependencies
+// local dependencies
 
 class App extends React.Component {
-//state and methods    
-    render() {
-        return (
-            <div>Hi</div>
-        )
-    }
+  render() {
+    return <div>Hi</div>;
+  }
 }
 
-ReactDOM.render(<App/>, root);
+ReactDOM.render(<App />, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-	template: __dirname + '/app/index.html',
-	filename: 'index.html',
-	inject: 'body'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: `${__dirname}/app/index.html`,
+  filename: 'index.html',
+  inject: 'body',
 });
 
 module.exports = {
-	entry: [
-		'./app/client.js'
-	],
-	output: {
-		path: __dirname + '/docs',
-		filename: "client_bundle.js"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: ['env']
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [HTMLWebpackPluginConfig],
-	mode: 'development'
-}
+  entry: ['./app/client.js'],
+  output: {
+    path: `${__dirname}/docs`,
+    filename: 'client_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [HTMLWebpackPluginConfig],
+  mode: 'development',
+};
